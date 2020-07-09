329071cb9cae1a75e62e86d767d198b303191fd4
jooohn <jooohn12341234@gmail.com>
2020-07-10 00:50:34 +0900

    Use serverless instead

--- serverless/handler.ts
+++ serverless/handler.ts
@@ -1,10 +1,22 @@
-import { DogEaredMain } from './adapters/dog-eared-main';
-import { SQS } from './adapters/sqs';
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { DogEaredMain } from './src/adapters/dog-eared-main';
+import { SQS } from './src/adapters/sqs';
+import 'source-map-support/register';
+
+export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
 
 export const startQuotedTweetsImport = async () => {
   const dogEaredMain = new DogEaredMain(env('GRAPHQL_URI'));
   const sqs = new SQS();
-  const queueUrl = env('QUOTED_TWEETS_IMPORT_QUEUE_URL');
+  const queueUrl = env('IMPORT_QUOTED_TWEETS_QUEUE_URL');
 
   const userIds = await dogEaredMain.fetchUserIds();
   const queueResult = await sqs.sendMessages(queueUrl, userIds.map(userId => ({
@@ -20,7 +32,7 @@ export const importQuotedTweets = async (event: any, context: any) => {
   console.log({ event, context });
 };
 
-function env<T extends string>(name: string): string {
+function env(name: string): string {
   const value = process.env[name];
   if (!value) {
     throw new Error(`${name} env must be specified`);
